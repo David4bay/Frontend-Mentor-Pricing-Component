e5bc20985252a201be7c0a0f83a0a0c6a7897b9c
David4bay <david4bay@gmail.com>
2023-02-06 20:44:45 +0100

    toggle test

--- script/script.js
+++ script/script.js
@@ -4,8 +4,3 @@ const toggle = document.getElementById("toggle");
 
 const basicAnnual = document.getElementById("basic");
 
-if (checkbox.checked === "true") {
-    toggle.style.left = "5px";
-} else {
-    toggle.style.right = "5px";
-}
